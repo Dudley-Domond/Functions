bc7e88cd3401e6547fe81e1e3a6eccf9f0704b09
Dudley D <94076825+dud93@users.noreply.github.com>
2021-12-13 01:54:54 -0500

    Update function_hw.js

--- function_hw.js
+++ function_hw.js
@@ -7,7 +7,6 @@ function maxOfThree(param1, param2, param3) {
 maxOfThree(10, 50, 100);
 
 
-
 //QUESTON 2a. Create a function called celsiusToFahrenheit:
 
 //- Take a Celsius temperature from the user and convert it to Fahrenheit
@@ -15,12 +14,17 @@ maxOfThree(10, 50, 100);
 //- Return string as output: "SOMETHING degrees Celsius is Something degrees Fahrenheit"
 
 
-let cels = param1 / 9/5 - 32;
-function celsiusToFahrenheit(param1) {
-    return (param1 * 9/5 + 32);
+function celsiusToFahrenheit(param1){
+        return param1;
 }
-console.log(cels "degrees celsiusis  is" param1 "degrees is" fahrenheit "ees fahrenheit")
-celsiusToFahrenheit(30);
+
+let prompt = require('prompt-sync')()
+
+let celsius = prompt("Enter a celsius value ");
+
+let fahrenheit = (celsius * 1.8) + 32
+
+console.log(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.`);
 
 
 //2b. Create a function called fahrenheitToCelsius:
@@ -30,6 +34,18 @@ celsiusToFahrenheit(30);
 //- Return string as output: "SOMETHING degrees Fahrenheit is SOMETHING degrees Celsius"
 
 
+function fahrenheitToCelsius(param1){
+    return param1;
+}
+
+let prompt = require('prompt-sync')()
+
+let fahrenheit = prompt("Enter fahrenheit value ");
+
+let celsius = (fahrenheit - 32) * 5 / 9
+
+console.log(`${fahrenheit} degree fahrenheit is equal to ${celsius} degree celsius.`);
+
 
 //3. Create an array of at least five Student objects with the following properties: name, activity, gender
 
@@ -38,5 +54,31 @@ celsiusToFahrenheit(30);
 //- Filter all those students who are in the activity entered by the user. For example, if the user enters "Basketball" display only students with basketball as their activity 
 
 
+let prompt = require('prompt-sync')()
+
+let user = prompt("Enter activity value ");
+
+const arr  = [
+    {name: "John", activity: "Swimming", gender: "Male"},
+    {name: "Tariq", activity: "Basketball", gender: "Male"},
+    {name: "Brianna", activity: "Painting", gender: "Female"},
+    {name: "Sasha", activity: "Singing", gender: "Female"},
+    {name: "Prince", activity: "Football", gender: "Male"},
+    {name: "Jeff", activity: "Bowling", gender: "Male"},
+    {name: "Diana", activity: "Tennis", gender: "Female"},
+
+];
+
+let x =  arr.filter(function(arr) {
+    return arr.activity == "Swimming";
+  });
+
+console.log(x);
+
 
 //QUESTION 4: Traverse through an array (either number or string array) and display each value with their index position using Array.map method
+
+
+const arr = ["Sincere", "Maurice", "Michelle", "Aaliyah", "Brittany",];
+
+arr.map((arr, index) => console.log(`The ${index} is ${arr}`));
