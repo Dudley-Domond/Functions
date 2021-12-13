2863508a5f0bcdc96422867e7d7176d2ac2ca32b
Dudley D <94076825+dud93@users.noreply.github.com>
2021-12-12 22:58:05 -0500

    Add files via upload

---
+++ function_hw.js
@@ -0,0 +1,42 @@
+//QUESTION 1: Define a function maxOfThree that takes three numbers as arguments and returns the largest of them.
+
+
+function maxOfThree(param1, param2, param3) {
+    console.log(Math.max(param1, param2, param3))
+}
+maxOfThree(10, 50, 100);
+
+
+
+//QUESTON 2a. Create a function called celsiusToFahrenheit:
+
+//- Take a Celsius temperature from the user and convert it to Fahrenheit
+
+//- Return string as output: "SOMETHING degrees Celsius is Something degrees Fahrenheit"
+
+
+let cels = param1 / 9/5 - 32;
+function celsiusToFahrenheit(param1) {
+    return (param1 * 9/5 + 32);
+}
+console.log(cels "degrees celsiusis  is" param1 "degrees is" fahrenheit "ees fahrenheit")
+celsiusToFahrenheit(30);
+
+
+//2b. Create a function called fahrenheitToCelsius:
+
+//- Now take a Fahrenheit temperature from the user and convert it to Celsius
+
+//- Return string as output: "SOMETHING degrees Fahrenheit is SOMETHING degrees Celsius"
+
+
+
+//3. Create an array of at least five Student objects with the following properties: name, activity, gender
+
+//- Filter all those students who are into swimming
+
+//- Filter all those students who are in the activity entered by the user. For example, if the user enters "Basketball" display only students with basketball as their activity 
+
+
+
+//QUESTION 4: Traverse through an array (either number or string array) and display each value with their index position using Array.map method
